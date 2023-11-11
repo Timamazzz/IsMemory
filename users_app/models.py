ace83b4881700ce29e1939b1d91c98ed2a22721c
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-11 13:01:04 +0300

    First commit

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -11,10 +12,51 @@ class Organization(models.Model):
         verbose_name_plural = "Организации"
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('email должен быть указан')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=True, null=True,
+    )
+
     patronymic = models.CharField(max_length=30, blank=True, null=True, verbose_name="Отчество")
+    email = models.EmailField(unique=True, blank=False, null=False, verbose_name="Email")
     organization = models.ForeignKey(Organization, on_delete=models.SET_NULL, blank=True, null=True,
                                      verbose_name="Организация")
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = "Пользователь"
