15193ad290d13dc97e081db2cbd9b3f0715095c2
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-19 20:07:54 +0300

    fix

--- deceased_app/views.py
+++ deceased_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import permissions
+
+from IsMemory.helpers.CustomModelViewSet import CustomModelViewSet
+from deceased_app.models import Deceased
+from deceased_app.serializers.deceased_serializers import DeceasedSerializer
+
 
 # Create your views here.
+class DeceasedViewSet(CustomModelViewSet):
+    queryset = Deceased.objects.all()
+    serializer_class = DeceasedSerializer
+    serializer_list = {
+        'retrieve': DeceasedSerializer,
+    }
+    permission_classes = [permissions.IsAuthenticated]
+
