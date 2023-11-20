708872d2c38f1c7113774503f7a69df51bea8147
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-20 10:52:43 +0300

    fix

--- locations_app/serializers/cemetery_serializers.py
+++ locations_app/serializers/cemetery_serializers.py
@@ -84,33 +84,35 @@ class CemeteryMapSerializer(serializers.ModelSerializer):
     def get_cemetery_plots(self, obj):
         request = self.context.get('request')
         print('context:', self.context)
-        statuses = request.query_params.get('status', None)
-        types = request.query_params.get('type', None)
+        if request:
+            statuses = request.query_params.get('status', None)
+            types = request.query_params.get('type', None)
 
-        ignore_filters = 'ignore_filters' in request.query_params
+            ignore_filters = 'ignore_filters' in request.query_params
 
-        cemetery_plots = CemeteryPlot.objects.filter(cemetery=obj)
+            cemetery_plots = CemeteryPlot.objects.filter(cemetery=obj)
 
-        if not ignore_filters:
-            statuses = statuses.split(',') if statuses else None
-            types = types.split(',') if types else None
+            if not ignore_filters:
+                statuses = statuses.split(',') if statuses else None
+                types = types.split(',') if types else None
 
-            status_filters = Q(status=None)
-            type_filters = Q(type=None)
+                status_filters = Q(status=None)
+                type_filters = Q(type=None)
 
-            if statuses:
-                status_filters = Q()
-                for status in statuses:
-                    status_filters |= Q(status=status)
+                if statuses:
+                    status_filters = Q()
+                    for status in statuses:
+                        status_filters |= Q(status=status)
 
-            if types:
-                type_filters = Q()
-                for type in types:
-                    type_filters |= Q(type=type)
+                if types:
+                    type_filters = Q()
+                    for type in types:
+                        type_filters |= Q(type=type)
 
-            cemetery_plots = cemetery_plots.filter(status_filters, type_filters)
+                cemetery_plots = cemetery_plots.filter(status_filters, type_filters)
 
-        return CemeteryMapSerializer(cemetery_plots, many=True).data
+            return CemeteryMapSerializer(cemetery_plots, many=True).data
+
 
     @staticmethod
     def get_for_free_plots_count(obj):
