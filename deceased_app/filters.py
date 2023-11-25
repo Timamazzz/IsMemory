ed92e88cab79852d39abc21207992871521b3aff
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-25 21:41:49 +0300

    add filters from deceased (fix v#2)

--- deceased_app/filters.py
+++ deceased_app/filters.py
@@ -10,17 +10,15 @@ class DeceasedFilter(filters.FilterSet):
     last_name = filters.CharFilter(lookup_expr='icontains', label='Фамилия')
     patronymic = filters.CharFilter(lookup_expr='icontains', label='Отчество')
 
-    birth_date__lte = filters.NumberFilter(field_name='birth_date', lookup_expr='year__lte',
-                                           label='Год рождения до')
-    death_date__lte = filters.NumberFilter(field_name='death_date', lookup_expr='year__lte',
-                                           label='Год смерти до')
+    birth_date = filters.NumberFilter(field_name='birth_date', lookup_expr='year__lte', label='Год рождения до')
+    death_date = filters.NumberFilter(field_name='death_date', lookup_expr='year__lte', label='Год смерти до')
 
     cemetery_plot = filters.CharFilter(field_name="cemetery_plot__cemetery__id", lookup_expr='exact',
                                        label='Кладбище')
 
     class Meta:
         model = Deceased
-        fields = ['first_name', 'last_name', 'patronymic', 'birth_date__lte', 'death_date__lte', 'cemetery_plot']
+        fields = ['first_name', 'last_name', 'patronymic', 'birth_date', 'death_date', 'cemetery_plot']
 
 
 class DeceasedFilterSerializer(serializers.Serializer):
@@ -28,12 +26,12 @@ class DeceasedFilterSerializer(serializers.Serializer):
     last_name = serializers.CharField(required=False, label='Фамилия')
     patronymic = serializers.CharField(required=False, label='Отчество')
 
-    birth_date__lte = serializers.DateField(required=False, label='Год рождения до')
-    death_date__lte = serializers.DateField(required=False, label='Год смерти до')
+    birth_date = serializers.DateField(required=False, label='Год рождения до')
+    death_date = serializers.DateField(required=False, label='Год смерти до')
 
     cemetery_plot = serializers.PrimaryKeyRelatedField(queryset=Cemetery.objects.all(), many=False, required=False,
                                                        label="Кладбище")
 
     class Meta:
         model = Deceased
-        fields = ['first_name', 'last_name', 'patronymic', 'birth_date__lte', 'death_date__lte', 'cemetery_plot']
+        fields = ['first_name', 'last_name', 'patronymic', 'birth_date', 'death_date', 'cemetery_plot']
