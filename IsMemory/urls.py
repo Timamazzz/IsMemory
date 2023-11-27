520913941169a9827d2b69b554577fd29c9301dd
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-11-27 17:24:53 +0300

    change admin url

--- IsMemory/urls.py
+++ IsMemory/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 urlpatterns = [
-    re_path(r'^django-admin/(.*)', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('api/users/', include('users_app.urls')),
     path('api/deceased/', include('deceased_app.urls')),
     path('api/locations/', include('locations_app.urls')),
