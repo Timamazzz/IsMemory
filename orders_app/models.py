515f6f695e5e98a6cb4f592f6c7b88ec71bd0474
Timama <timforworking@mail.ru>
2023-12-17 22:07:04 +0300

    orders_app(fix)

---
+++ orders_app/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from deceased_app.models import Deceased
+from orders_app.enums import OrderStatusEnum
+from services_app.models import Service
+from users_app.models import PhoneNumberValidator
+
+
+class Executor(models.Model):
+    first_name = models.CharField(max_length=255,  null=True, blank=True, verbose_name='Имя исполнителя')
+    last_name = models.CharField(max_length=255,  null=True, blank=True, verbose_name='Фамилия исполнителя')
+    patronymic = models.CharField(max_length=255,  null=True, blank=True, verbose_name='Отчество исполнителя')
+    phone_number = models.CharField(
+        validators=[PhoneNumberValidator()],
+        max_length=17,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name="Номер телефона"
+    )
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.patronymic} - {self.phone_number}'
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название заказа')
+    date = models.DateField(verbose_name='Дата заказа', auto_now_add=True)
+
+    executor = models.ForeignKey(Executor, on_delete=models.SET_NULL, null=True, blank=True,
+                                 verbose_name='Исполнитель')
+
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name='Услуга', null=True, blank=True)
+    deceased = models.ForeignKey(Deceased, on_delete=models.CASCADE, verbose_name='Усопший', null=True, blank=True)
+
+    comment = models.TextField(blank=True, null=True, verbose_name='Комментарий')
+    is_good = models.BooleanField(default=False, verbose_name='Все хорошо')
+    is_bad = models.BooleanField(default=False, verbose_name='Есть замечания')
+
+    status = models.CharField(
+        max_length=20,
+        choices=[(status.name, status.value) for status in OrderStatusEnum],
+        default=OrderStatusEnum.AWAITING_PAYMENT.name,
+        verbose_name='Статус',
+        null=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = f'Заказ №{self.id} {self.date.strftime("%d-%m-%Y")}'
+        super(Order, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
