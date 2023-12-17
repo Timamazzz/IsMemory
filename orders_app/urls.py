8fd7f22faac329f333d3c0af205a795085708e0b
Timama <timforworking@mail.ru>
2023-12-18 00:03:25 +0300

    fix

---
+++ orders_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from orders_app.views import OrderViewSet
+
+router = DefaultRouter()
+router.register(r'', OrderViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
