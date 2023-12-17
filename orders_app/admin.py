3cebbb46a7acbbce443cb6cd704d0f32d72d300d
Timama <timforworking@mail.ru>
2023-12-17 22:19:49 +0300

    add admin

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Executor, Order
 
-# Register your models here.
+
+@admin.register(Executor)
+class ExecutorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'patronymic', 'phone_number')
+    search_fields = ('first_name', 'last_name', 'patronymic', 'phone_number')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'executor', 'service', 'deceased', 'is_good', 'is_bad', 'status')
+    search_fields = ('name', 'comment')
