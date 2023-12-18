7da69c57d8b24f8fbf85f0311920613e840ad7cb
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-12-18 15:48:14 +0300

    upload one file

--- orders_app/views.py
+++ orders_app/views.py
@@ -77,6 +77,7 @@ class ExecutorViewSet(CustomModelViewSet):
     @action(detail=False, methods=['PUT'])
     def set_data(self, request, *args, **kwargs):
         phone_number = request.data.get('phone_number')
+        print('phone', phone_number)
         chat_id = request.data.get('chat_id')
         try:
             executor = Executor.objects.get(phone_number=phone_number)
