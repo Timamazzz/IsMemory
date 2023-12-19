f130332ba8cb7c6cf71c29a68eea75810748f71f
Timama <76005360+Timamazzz@users.noreply.github.com>
2023-12-19 14:15:30 +0300

    upload one file

--- bot.py
+++ bot.py
@@ -154,9 +154,6 @@ async def finish_order(message: types.Message, state: FSMContext):
     order_id = order_data.get('order_id')
     images = order_data.get('images', [])
     await message.answer(
-        f"order_id: {order_id}, images:{images}",
-    )
-    await message.answer(
         "Для завершения заказа, прикрепите изображение(я) с выполненной работой.",
         reply_markup=finish_keyboard,
     )
@@ -168,9 +165,6 @@ async def process_finish_order(message: types.Message, state: FSMContext):
     order_id = order_data.get('order_id')
     images = order_data.get('images', [])
 
-    order_data = await state.get_data()
-    order_id = order_data.get('order_id')
-    images = order_data.get('images', [])
     await message.answer(
         f"order_id: {order_id}, images:{images}",
     )
@@ -204,10 +198,6 @@ async def handle_completed_order(message: types.Message, state: FSMContext):
     if order_id:
         images = order_data.get('images', [])
 
-        await message.answer(
-            f"1",
-        )
-
         file_info = await bot.get_file(message.photo[0].file_id)
         file_path = file_info.file_path
 
@@ -217,23 +207,19 @@ async def handle_completed_order(message: types.Message, state: FSMContext):
         response = requests.get(file_url)
         file_name = f"{message.photo[0].file_id}{file_extension}"
 
-        await message.answer(
-            f"2",
-        )
-
         if response.status_code == 200:
             with open(os.path.join('media', file_name), 'wb') as file:
                 file.write(response.content)
 
             await message.answer(
-                f"3",
+                f"before state update order_id: {order_id}, images:{images}",
             )
 
             images.append({"file": file_name, "original_name": file_path})
             await state.update_data(images=images)
 
             await message.answer(
-                f"order_id: {order_id}, images:{images}",
+                f"after state update order_id: {order_id}, images:{images}",
             )
         else:
             await message.answer("Ошибка отправки фото")
