624a01fb6bdf7967f9917f27fc01eb5f600554c2
Timama <76005360+Timamazzz@users.noreply.github.com>
2024-01-11 10:10:24 +0300

    add multiple price in services

--- services_app/models.py
+++ services_app/models.py
@@ -7,6 +7,9 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     description = models.TextField(verbose_name='Описание')
 
+    is_multiple_price = models.BooleanField(default=False, verbose_name='Множественная цена')
+    count = models.PositiveIntegerField(default=1, verbose_name='Количество')
+
     class Meta:
         verbose_name = 'Услуга'
         verbose_name_plural = 'Услуги'
