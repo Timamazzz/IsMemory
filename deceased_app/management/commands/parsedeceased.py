d78cd32dee088376c03d641ab187c8180cc517dc
timama <timforworking@mail.ru>
2024-02-19 12:50:29 +0300

    fix

--- deceased_app/management/commands/parsedeceased.py
+++ deceased_app/management/commands/parsedeceased.py
@@ -21,7 +21,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            cemetery = Cemetery.objects.get(pk=8)
+            cemetery = Cemetery.objects.get(name='Ячнево')
+
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/91.0.4472.124 Safari/537.36'
