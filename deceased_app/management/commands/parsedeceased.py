420e290260f2f165ffe50a65e6a43d8b08c5b010
timama <timforworking@mail.ru>
2024-02-20 18:16:04 +0300

    fix

--- deceased_app/management/commands/parsedeceased.py
+++ deceased_app/management/commands/parsedeceased.py
@@ -131,16 +131,16 @@ class Command(BaseCommand):
                                 plot_images.append(plot_image)
 
             self.stdout.write(self.style.SUCCESS(f'deceased_objects: {deceased_objects}'))
-            Deceased.objects.bulk_create(deceased_objects)
-            loaded_deceased += len(deceased_objects)
+            created_deceased = Deceased.objects.bulk_create(deceased_objects)
+            loaded_deceased += len(created_deceased)
 
             self.stdout.write(self.style.SUCCESS(f'plot_objects: {plot_objects}'))
-            CemeteryPlot.objects.bulk_create(plot_objects)
-            loaded_plots += len(plot_objects)
+            created_plots = CemeteryPlot.objects.bulk_create(plot_objects)
+            loaded_plots += len(created_plots)
 
-            for plot, deceased in zip(plot_objects, deceased_objects):
+            for plot, deceased in zip(created_plots, created_deceased):
                 deceased.cemetery_plot = plot
-            Deceased.objects.bulk_update(deceased_objects, ['cemetery_plot'])
+            Deceased.objects.bulk_update(created_deceased, ['cemetery_plot'])
 
             self.stdout.write(self.style.SUCCESS(f'plot_images: {plot_images}'))
             CemeteryPlotImage.objects.bulk_create(plot_images)
