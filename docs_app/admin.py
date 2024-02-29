009b64e444a4a74d9a70c0b8e1eb85b430f728cf
timama <timforworking@mail.ru>
2024-02-29 13:02:33 +0300

    fix

--- docs_app/admin.py
+++ docs_app/admin.py
@@ -5,7 +5,7 @@ from docs_app.models import CemeteryPlotImage
 
 # Register your models here.
 class CemeteryPlotImageAdmin(admin.ModelAdmin):
-    list_display = ('cemetery_plot', 'file', 'uploaded_at', 'original_name')
+    list_display = ('id', 'cemetery_plot', 'file', 'uploaded_at', 'original_name', 'is_preview')
     search_fields = ('cemetery_plot__name', 'original_name')
     list_filter = ('cemetery_plot',)
     date_hierarchy = 'uploaded_at'
