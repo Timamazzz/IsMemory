97e27d5dab8e0971ad15e8fd0429b99c395c3abf
Timama <timforworking@mail.ru>
2024-03-03 12:02:08 +0300

    fix

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from orders_app.enums import OrderStatusEnum
 
-BOT_TOKEN = '6845960244:AAEqZSwtsNb3zaj2uDtZ6HplPPzYPaFB28U'
+#BOT_TOKEN = '6845960244:AAEqZSwtsNb3zaj2uDtZ6HplPPzYPaFB28U'
+BOT_TOKEN = '7115409811:AAEGwdzxIiXhXuKJDefBgm8vzeB7qR-oOp8'
 API_URL = "https://belmemorial.ru/api"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 YaBrowser/24.1.0.0 Safari/537.36',
